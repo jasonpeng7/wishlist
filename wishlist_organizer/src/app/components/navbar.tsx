128c38beb40cd63a623644369bc770dacf7a40aa
jasonpeng7 <144397187+jasonpeng7@users.noreply.github.com>
2024-12-14 02:08:51 -0800

    edit

--- wishlist_organizer/src/app/components/navbar.tsx
+++ wishlist_organizer/src/app/components/navbar.tsx
@@ -19,20 +19,20 @@ export default function NavBar () {
               priority>
       </Image>
     </div>
-    <div className='flex gap-x-[25px]'>
-      <Link 
-          href={pathName === "/wishlists" ? "/dashboard" : pathName === "/groups" ? "/dashboard" : "/wishlists"}
-          className="bg-primary_text px-4 py-2 rounded-full transition-colors text-xs h-fit"
-      >
-          {pathName.includes("/wishlists") ? "Dashboard" : pathName.includes("/groups") ? "Dashboard": "All Wishlists"}
-      </Link>
-      <Link
-          href={pathName === "/groups" ? "/wishlists": "/groups"}
-          className='bg-primary_text px-4 py-2 rounded-full transition-colors text-xs h-fit'
-      >
-          {pathName.includes("/groups") ? "All Wishlists" : "Your Group"}  
-      </Link>
-      <UserButton/>
+    <div className='flex gap-x-[25px] items-center'>
+        <Link 
+            href={pathName === "/wishlists" ? "/dashboard" : pathName === "/groups" ? "/dashboard" : "/wishlists"}
+            className="bg-primary_text px-4 py-2 rounded-full transition-colors text-xs flex items-center h-[32px]"
+        >
+            {pathName.includes("/wishlists") ? "Dashboard" : pathName.includes("/groups") ? "Dashboard": "All Wishlists"}
+        </Link>
+        <Link
+            href={pathName === "/groups" ? "/wishlists": "/groups"}
+            className='bg-primary_text px-4 py-2 rounded-full transition-colors text-xs flex items-center h-[32px]'
+        >
+            {pathName.includes("/groups") ? "All Wishlists" : "Your Group"}  
+        </Link>
+        <UserButton/>
     </div>
 </nav>
     );
