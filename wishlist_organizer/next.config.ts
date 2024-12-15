fb2c017b844c115d6e064654f22652d84c1e91f1
jasonpeng7 <144397187+jasonpeng7@users.noreply.github.com>
2024-12-14 18:21:59 -0800

    edit

--- wishlist_organizer/next.config.ts
+++ wishlist_organizer/next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const config: NextConfig = {
+  reactStrictMode: true,
+  distDir: '.next',
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "2mb",
+      allowedOrigins: ["*"]
+    }
+  },
+  // Add ESLint config
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  pageExtensions: ['ts', 'tsx', 'mdx']
 };
 
-export default nextConfig;
+export default config;
