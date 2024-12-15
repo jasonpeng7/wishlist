7ca4bd20e0cbd2dc10aa659ba7c568fa6e78c813
jasonpeng7 <144397187+jasonpeng7@users.noreply.github.com>
2024-12-14 22:01:48 -0800

    edited key

--- wishlist_organizer/utils/supabase.ts
+++ wishlist_organizer/utils/supabase.ts
@@ -1,10 +1,10 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl: string = 'https://udeoscdqsqggqmsutaor.supabase.co'
-const supabaseKey: string = process.env.SUPABASE_KEY as string
+const supabaseKey: string = process.env.NEXT_PUBLIC_SUPABASE_KEY as string
 
 if (!supabaseKey) {
-  throw new Error('Missing SUPABASE_KEY environment variable')
+  throw new Error('Missing NEXT_PUBLIC_SUPABASE_KEY environment variable')
 }
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
