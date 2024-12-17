1d8108804f7c992ea6156dd5b6e7ecfc6981dc10
jasonpeng7 <144397187+jasonpeng7@users.noreply.github.com>
2024-12-17 14:55:58 -0800

    fixed center alignment, added functionality to logo

--- wishlist_organizer/src/app/components/navbar.tsx
+++ wishlist_organizer/src/app/components/navbar.tsx
@@ -21,13 +21,19 @@ export default function NavBar () {
     return(
     <nav className="fixed w-full flex justify-between gap-x-[25px] items-center bg-washed_gray px-[20px] h-[60px] z-50">
     <div className='flex-shrink-0'>
-      <Image   
-              src="/logo.svg" 
-              alt="Logo"
-              width={40}
-              height={40}
-              priority>
-      </Image>
+    <Link
+    href="/dashboard"
+    className="flex transition-transform transform active:scale-90"
+    onClick={pathName === "/dashboard" ? undefined: handleClick}>
+        <Image   
+                src="/logo.svg" 
+                alt="Logo"
+                width={40}
+                height={40}
+                priority>
+        </Image>
+    </Link>
+
     </div>
     <div className='flex gap-x-[25px] items-center font-raleway font-medium'>
         <Link      
@@ -65,13 +71,14 @@ export default function NavBar () {
             pathName.includes("/groups") ? "Your Group" : 
             "Your Group"}
          </Link>
-        <UserButton/>
         {modal && (
         <div className="fixed top-0 left-0 w-screen h-screen bg-black opacity-50 flex justify-center items-center">
             <Loader/>
         </div>
         )}
     </div>
+    <UserButton/>
+
 </nav>
     );
 }
