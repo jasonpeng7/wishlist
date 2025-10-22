a5a4d2467472de4d114979683c3709b58b59be0d
jasonpeng7 <144397187+jasonpeng7@users.noreply.github.com>
2025-10-21 21:29:29 -0700

    universal loading component as data fetches

---
+++ wishlist_organizer/src/app/components/Loader.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Loader() {
+  return (
+    <div className="flex items-center justify-center h-full">
+      <div className="w-16 h-16 border-4 border-t-transparent border-bone rounded-full animate-spin"></div>
+    </div>
+  );
+}
