d2e35ed10936bc96dedb6d6a3babb8c9b956490b
jasonpeng7 <144397187+jasonpeng7@users.noreply.github.com>
2025-10-21 21:39:14 -0700

    .

--- wishlist_organizer/src/app/groups/page.tsx
+++ wishlist_organizer/src/app/groups/page.tsx
@@ -1,8 +1,8 @@
-import { currentUser } from '@clerk/nextjs/server';
-import { redirect } from 'next/navigation';
-import { supabase } from '../../../utils/supabase';
-import Link from 'next/link';
-import NavBar from '@/app/components/navbar';
+import { supabase } from "../../../utils/supabase";
+import Link from "next/link";
+import NavBar from "@/app/components/navbar";
+import { getSessionUser } from "../../../../wishlist_organizer/utils/auth";
+import { redirect } from "next/navigation";
 
 interface UserGroup {
   user_id: string;
@@ -27,40 +27,32 @@ interface MemberGroupResponse {
 }
 
 export default async function GroupsPage() {
-  const user = await currentUser();
-  
+  const user = await getSessionUser();
+
   if (!user) {
-    redirect('/sign-in');
+    redirect("/signin");
   }
 
-  // Fetch groups where user is admin
-  const { data: adminGroups } = await supabase
-    .from('groups')
-    .select(`
-      *,
-      user_groups (
-        user_id
-      )
-    `)
-    .eq('creator_id', user.id) as { data: GroupDetails[] | null };
+  // run all queries in parallel
+  const adminGroupsPromise = supabase
+    .from("groups")
+    .select(`*, user_groups(user_id)`)
+    .eq("creator_id", user.id);
 
-  // Fetch groups where user is a member
-  const { data: memberGroups } = await supabase
-    .from('user_groups')
-    .select(`
-      group:groups (
-        *,
-        user_groups (
-          user_id
-        )
-      )
-    `)
-    .eq('user_id', user.id)
-    .eq('role', 'member') as { data: MemberGroupResponse[] | null };
+  const memberGroupsPromise = supabase
+    .from("user_groups")
+    .select(`group:groups (*,user_groups (user_id))`)
+    .eq("user_id", user.id)
+    .eq("role", "member");
+
+  const [{ data: adminGroups }, { data: memberGroups }] = await Promise.all([
+    adminGroupsPromise,
+    memberGroupsPromise,
+  ]);
 
   return (
     <div className="">
-      <NavBar/>
+      <NavBar />
       <div className="font-raleway flex flex-col md:flex-row justify-between items-center mb-8 pt-[100px] md: mx-[50px] md:mx-[100px] text-primary_text">
         <h1 className="text-2xl font-bold mb-4 md:mb-0">My Groups</h1>
       </div>
@@ -68,15 +60,15 @@ export default async function GroupsPage() {
       {/* Groups I Manage */}
       <section className="mb-8 mx-[50px] md:mx-[100px] font-raleway text-primary_text">
         <h2 className="text-xl font-semibold mb-4">Groups I Manage</h2>
-        <div className='flex mb-4 '>
-          <Link              
-                href="/groups/create"         
-                className="bg-bone px-4 py-2 rounded-lg 
+        <div className="flex mb-4 ">
+          <Link
+            href="/groups/create"
+            className="bg-bone px-4 py-2 rounded-lg 
                 transition-transform transform active:scale-90 
-                flex items-center justify-center text-center"         
-                >             
-                <p className='text-dark_gray'>Create a group</p> 
-            </Link>
+                flex items-center justify-center text-center"
+          >
+            <p className="text-dark_gray">Create a group</p>
+          </Link>
         </div>
         <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
           {adminGroups?.map((group) => (
@@ -102,36 +94,45 @@ export default async function GroupsPage() {
                   View Wishlists
                 </Link>
               </div>
-
             </div>
           ))}
         </div>
         {(!adminGroups || adminGroups.length === 0) && (
-          <p className="text-gray-500">You haven't created any groups yet. Create one before you start making a wishlist!</p>
+          <p className="text-gray-500">
+            You haven&apos;t created any groups yet. Create one before you start
+            making a wishlist!
+          </p>
         )}
       </section>
 
       {/* Groups I'm In */}
-      <section className='mx-[50px] md:mx-[100px] font-raleway mb-[50px]'>
-        <h2 className="text-xl font-semibold mb-4 text-primary_text">Groups I'm In</h2>
-        <div className='flex mb-4'>
-          <Link              
-              href="/groups/join"         
-              className="bg-bone px-4 py-2 rounded-lg 
+      <section className="mx-[50px] md:mx-[100px] font-raleway mb-[50px]">
+        <h2 className="text-xl font-semibold mb-4 text-primary_text">
+          Groups I&apos;m In
+        </h2>
+        <div className="flex mb-4">
+          <Link
+            href="/groups/join"
+            className="bg-bone px-4 py-2 rounded-lg 
               transition-transform transform active:scale-90 
-              flex items-center justify-center"         
-              >             
-              <p className='text-dark_gray'>Join a group</p> 
+              flex items-center justify-center"
+          >
+            <p className="text-dark_gray">Join a group</p>
           </Link>
         </div>
         <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
           {memberGroups?.map((memberGroup) => (
-            <div key={memberGroup.group.id} className="bg-midnight_blue p-4 rounded-lg">
-              <h3 className="font-medium text-primary_text">{memberGroup.group.name}</h3>
+            <div
+              key={memberGroup.group.id}
+              className="bg-midnight_blue p-4 rounded-lg"
+            >
+              <h3 className="font-medium text-primary_text">
+                {memberGroup.group.name}
+              </h3>
               <p className="text-sm text-primary_text">
                 Members: {memberGroup.group.user_groups?.length || 0}
               </p>
-              <div className="mt-4 space-x-4 flex justify-center lg:justify-start"> 
+              <div className="mt-4 space-x-4 flex justify-center lg:justify-start">
                 <Link
                   href={`/groups/${memberGroup.group.id}`}
                   className="flex w-1/3 text-xs bg-washed_gray text-primary_text hover:underline rounded px-4 py-2
@@ -152,7 +153,10 @@ export default async function GroupsPage() {
           ))}
         </div>
         {(!memberGroups || memberGroups.length === 0) && (
-          <p className="text-gray-500">You're not a member of any groups yet. Join one before making your wishlist!</p>
+          <p className="text-gray-500">
+            You&apos;re not a member of any groups yet. Join one before making
+            your wishlist!
+          </p>
         )}
       </section>
     </div>
