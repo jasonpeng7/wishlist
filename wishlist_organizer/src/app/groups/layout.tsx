20f62fc627d7cf564c240e572ca0299011a31b31
jasonpeng7 <144397187+jasonpeng7@users.noreply.github.com>
2025-10-21 21:38:48 -0700

    navbar for groups page

---
+++ wishlist_organizer/src/app/groups/layout.tsx
@@ -0,0 +1,14 @@
+import NavBar from "../components/navbar";
+
+export default function GroupsLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div>
+      <NavBar />
+      {children}
+    </div>
+  );
+}
