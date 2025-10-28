eb2d9c53b7406b5a8d3f8d8703a34572a96649a9
jasonpeng7 <jiapeng@ucdavis.edu>
2025-10-28 01:17:45 -0700

    .

---
+++ wishlist_organizer/utils/supabase/client.ts
@@ -0,0 +1,15 @@
+import { createClient as createSupabaseClient } from '@supabase/supabase-js'
+
+export function createClient(token: string) {
+  return createSupabaseClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      global: {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      }
+    }
+  )
+}
