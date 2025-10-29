64c378ee9d636dbf3f7ffc071a6d1d3d46499239
jasonpeng7 <144397187+jasonpeng7@users.noreply.github.com>
2025-10-29 00:38:18 -0700

    frontend for sign up

--- wishlist_organizer/src/app/signup/page.tsx
+++ wishlist_organizer/src/app/signup/page.tsx
@@ -30,52 +30,52 @@ export default function SignUpPage() {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-slate_gray">
-      <div className="w-full max-w-md p-8 space-y-6 bg-washed_gray rounded-lg shadow-md">
-        <h1 className="text-2xl font-bold text-center text-primary_text">
+    <div className="flex items-center justify-center min-h-screen bg-primary_text">
+      <div className="w-full max-w-md p-8 space-y-6 bg-[#f7f9fb] mt-20 glass-element min-h-screen rounded-t-3xl">
+        <h1 className="text-3xl font-bold text-center text-white">
           Create an Account
         </h1>
         <form onSubmit={handleSubmit} className="space-y-6">
           <div>
-            <label className="block text-sm font-medium text-primary_text">
-              Email
+            <label className="block text-sm font-medium text-white">
+              Email*
             </label>
             <input
               type="text"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
-              className="w-full p-2 mt-1 border rounded bg-primary_text text-dark_gray"
+              className="w-full p-2 mt-1 border rounded bg-white text-primary_text"
               required
             />
           </div>
           <div>
-            <label className="block text-sm font-medium text-primary_text">
-              Username
+            <label className="block text-sm font-medium text-white">
+              Username*
             </label>
             <input
               type="text"
               value={username}
               onChange={(e) => setUsername(e.target.value)}
-              className="w-full p-2 mt-1 border rounded bg-primary_text text-dark_gray"
+              className="w-full p-2 mt-1 border rounded bg-white text-primary_text"
               required
             />
           </div>
           <div>
-            <label className="block text-sm font-medium text-primary_text">
-              Password
+            <label className="block text-sm font-medium text-white">
+              Password*
             </label>
             <input
-              type="password"
+              type="text"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
-              className="w-full p-2 mt-1 border rounded bg-primary_text text-dark_gray"
+              className="w-full p-2 mt-1 border rounded bg-white text-primary_text"
               required
             />
           </div>
           {error && <p className="text-sm text-red-500">{error}</p>}
           <button
             type="submit"
-            className="w-full px-4 py-2 font-bold text-white bg-midnight_blue rounded-md hover:bg-dark_gray"
+            className="w-full px-4 py-2 font-bold text-primary_text bg-bone rounded-md"
           >
             Sign Up
           </button>
