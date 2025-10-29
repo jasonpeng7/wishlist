896267f9d6b8e07d047175c3811eab31c8e1247a
jasonpeng7 <144397187+jasonpeng7@users.noreply.github.com>
2025-10-29 00:38:55 -0700

    changed root bg

--- wishlist_organizer/src/app/loading.tsx
+++ wishlist_organizer/src/app/loading.tsx
@@ -2,7 +2,7 @@ import Loader from "./components/Loader";
 
 export default function Loading() {
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-slate_gray">
+    <div className="fixed inset-0 z-50 flex items-center justify-center christmas-stripes">
       <Loader />
     </div>
   );
