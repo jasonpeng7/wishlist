8af17b6b1f33cc44550524f8e13a14c1145e14db
jasonpeng7 <144397187+jasonpeng7@users.noreply.github.com>
2025-12-09 02:15:21 -0800

    used snowfall loader

--- wishlist_organizer/src/app/loading.tsx
+++ wishlist_organizer/src/app/loading.tsx
@@ -1,9 +1,10 @@
-import Loader from "./components/Loader";
+import Snowfall from "./components/Snowfall";
 
 export default function Loading() {
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center christmas-stripes">
-      <Loader />
+    <div className="fixed inset-0 z-50 bg-[#090a0f] flex items-center justify-center">
+      <Snowfall />
+      <h1 className="text-white font-raleway text-2xl animate-pulse z-10">Loading...</h1>
     </div>
   );
 }
